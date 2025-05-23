94c394ea82172046a766a95288c4ab50c6489e56
Gabriel Rosero <lganama.4345@unicesmag.edu.co>
2025-05-23 18:01:50 -0500

    Create photo default useres

--- src/list_users.php
+++ src/list_users.php
@@ -17,6 +17,7 @@ include('../config/database.php');
             <th>Lastname</th>
             <th>E_mail</th>
             <th>Status</th>
+            <th>Photo</th>
             <td>...</td>
         </tr>
         
@@ -25,7 +26,7 @@ include('../config/database.php');
 
         <?php
         //Here Code
-        $sql="select firstname, lastname, email, case when status = true then 'Active' else 'no active' end as status
+        $sql="select id, firstname, lastname, email, case when status = true then 'Active' else 'no active' end as status
 	    from users";
         $res = pg_query($conn, $sql);
         if(!$res){
@@ -38,10 +39,11 @@ include('../config/database.php');
             echo "<td>".$row['lastname']."</td>";
             echo "<td>".$row['email']."</td>";
             echo "<td>".$row['status']."</td>";
+            echo "<td align='center'><img src='photo_users/photo_default.png'width='20'></td>";
             echo "<td>";
             echo "<a href=''><img src='icons/usuario.png' width='15'></a>";
-            echo "<a href=''><img src='icons/delete.png' width='15'></a>";
             echo "<a href=''><img src='icons/search.png' width='15'></a>";
+            echo "<a href='http://127.0.0.1/schoolar/src/delete.php'><img src='icons/delete.png' width='15'></a>";
             echo "</td>";
             echo "</tr>";
         }
